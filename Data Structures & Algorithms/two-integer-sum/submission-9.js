0f8f8d8bff34826e49bc1802b86ed4a1520597da
Fred Gatus <22206381+hit1st@users.noreply.github.com>
2026-08-15 15:08:47 -0700

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const hashMap = new Map();
+        
+        for (let i = 0; i < nums.length; i += 1) {
+            const diff = target - nums[i];
+            if (hashMap.has(diff)) return [hashMap.get(diff), i];
+            hashMap.set(nums[i], i);
+        }
+        return [];
+    }
+}
