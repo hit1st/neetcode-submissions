91d3e52f049a75b9e6721034069926e0526a6b2f
Fred Gatus <22206381+hit1st@users.noreply.github.com>
2026-08-20 13:54:13 -0700

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board) {
+        const rows = new Map();
+        const cols = new Map();
+        const squares = new Map();
+
+        for (let row = 0; row < 9; row += 1) {
+            for (let col = 0; col < 9; col += 1) {
+                const boxValue = board[row][col];
+                if (boxValue === '.') continue;
+                
+                const key = `${Math.floor(row / 3)},${Math.floor(col / 3)}`;
+
+                console.log('rows: ', rows);
+                console.log('cols: ', cols);
+                console.log('squares: ', squares);
+                if (
+                    rows.has(row) && rows.get(row).has(boxValue) ||
+                    cols.has(col) && cols.get(col).has(boxValue) ||
+                    squares.has(key) && squares.get(key).has(boxValue)
+                ) return false;
+        
+
+                if (!rows.has(row)) rows.set(row, new Set());
+                if (!cols.has(col)) cols.set(col, new Set());
+                if (!squares.has(key)) squares.set(key, new Set());
+
+                rows.get(row).add(boxValue);
+                cols.get(col).add(boxValue);
+                squares.get(key).add(boxValue);
+            }
+        }
+
+        return true;
+    }
+}
