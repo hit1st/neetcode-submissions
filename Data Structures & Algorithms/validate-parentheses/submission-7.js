d1a96343920b68b5a4f63eeaf735d2f8d8e01dc2
Fred Gatus <22206381+hit1st@users.noreply.github.com>
2026-08-26 17:22:22 -0700

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stack = [];
+        const closeMatch = {
+            ')': '(',
+            '}': '{',
+            ']': '[',
+        };
+        
+        for (const bracket of s) {
+            if (closeMatch[bracket]) {
+            if (stack.length > 0 && stack[stack.length - 1] === closeMatch[bracket]) stack.pop();
+            else return false;
+            } else {
+            stack.push(bracket);
+            }
+        }
+        
+        return stack.length === 0;
+    }
+}
