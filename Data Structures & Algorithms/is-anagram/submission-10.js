3823b1bb765143a5df0f3cd383ca814b99dc7e33
Fred Gatus <22206381+hit1st@users.noreply.github.com>
2026-08-29 13:31:06 -0700

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) return false;
+  
+        const count = Array(26).fill(0);
+        
+        for (let i = 0; i < s.length; i += 1) {
+            count[s.charCodeAt(i) - 97] += 1;
+            count[t.charCodeAt(i) - 97] -= 1;
+        }
+        
+        return count.every(num => num === 0);
+    }
+}
