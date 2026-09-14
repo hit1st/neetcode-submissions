beb998f88912382988c1437ef7185fba052db8a7
Fred Gatus <22206381+hit1st@users.noreply.github.com>
2026-09-14 12:19:31 -0700

    Add: permutation-string - submission-8

---
+++ Data Structures & Algorithms/permutation-string/submission-8.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    checkInclusion(s1, s2) {
+        if (s2.length < s1.length) return false;
+        const s1Count = Array(26).fill(0);
+        const s2Count = Array(26).fill(0);
+        for (let i = 0; i < s1.length; i += 1) {
+            s1Count[s1[i].charCodeAt(0) - 97] += 1;
+            s2Count[s2[i].charCodeAt(0) - 97] += 1;
+        }
+        
+        const s1CountStr = JSON.stringify(s1Count);
+
+        for (let i = 0; i < s2.length - s1.length + 1; i += 1) {
+            if (s1CountStr === JSON.stringify(s2Count)) return true;
+            if (i + s1.length < s2.length) {
+                s2Count[s2[i].charCodeAt(0) - 97] -= 1;
+                s2Count[s2[i + s1.length].charCodeAt(0) - 97] += 1;
+            }
+        }
+
+        return false;
+    }
+}
