44697cbd403b4ebffb0cd041eca2677c8333ea43
Fred Gatus <22206381+hit1st@users.noreply.github.com>
2026-09-17 11:49:30 -0700

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        const count = new Map();
+        let longest = 0;
+        let mostFreq = 0;
+        let l = 0;
+
+        for (let r = 0; r < s.length; r += 1) {
+            count.set(s[r], (count.get(s[r]) || 0) + 1);
+            mostFreq = Math.max(mostFreq, count.get(s[r]));
+            if (r - l + 1 - mostFreq > k) {
+                count.set(s[l], (count.get(s[l] - 1)));
+                l += 1;
+            }
+            longest = Math.max(longest, r - l + 1);
+        }
+
+        return longest;
+    }
+}
