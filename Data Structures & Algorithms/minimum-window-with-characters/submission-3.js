ff6b707b518703c096cb6fc73f4bd4e15ad680f6
Fred Gatus <22206381+hit1st@users.noreply.github.com>
2026-09-23 13:03:39 -0700

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {string}
+     */
+    minWindow(s, t) {
+        if (s.length < t.length) return '';
+
+        const tCount = {};
+        const window = {};
+        for (const c of t) {
+            tCount[c] = (tCount[c] || 0) + 1;
+        }
+
+        const need = Object.keys(tCount).length;
+        let have = 0;
+        let res = [-1, -1];
+        let resLen = Infinity;
+        let l = 0;
+
+        for (let r = 0; r < s.length; r += 1) {
+            const ch = s[r];
+            window[ch] = (window[ch] || 0) + 1;
+            if (tCount[ch] && tCount[ch] === window[ch]) have += 1;
+
+            while (need === have) {
+                if (r - l + 1 < resLen) {
+                    resLen = r - l + 1;
+                    res = [l, r];
+                }
+                window[s[l]] -= 1;
+                if (tCount[s[l]] && window[s[l]] < tCount[s[l]]) have -= 1;
+                l += 1;
+            }
+        }
+
+        return resLen === Infinity ? '' : s.slice(res[0], res[1] + 1);
+    }
+}
