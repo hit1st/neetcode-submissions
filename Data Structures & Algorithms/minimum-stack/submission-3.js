3c2adacfd1c509d25498b981b641f19a63ba7dca
Fred Gatus <22206381+hit1st@users.noreply.github.com>
2026-09-24 10:07:03 -0700

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.js
@@ -0,0 +1,38 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.min = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+        const min = Math.min
+        this.min.push(Math.min((this.min.length > 0 ? this.min[this.min.length - 1] : Infinity), val));
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.min.pop();
+        return this.stack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.min[this.min.length - 1];
+    }
+}
