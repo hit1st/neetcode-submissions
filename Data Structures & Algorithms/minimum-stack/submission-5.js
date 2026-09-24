9dfae9efeaef5c3ca73f163e29a734fd4697a651
Fred Gatus <22206381+hit1st@users.noreply.github.com>
2026-09-24 10:09:50 -0700

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.js
@@ -0,0 +1,37 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.min = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+        this.min.push(Math.min((this.min.length > 0 ? this.min[this.min.length - 1] : val), val));
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.min.pop();
+        return this.stack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.min[this.min.length - 1];
+    }
+}
