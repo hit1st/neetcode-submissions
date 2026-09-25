89d3a7246ee75a3d8c50acb7b8bab175835e5de9
Fred Gatus <22206381+hit1st@users.noreply.github.com>
2026-09-25 12:14:05 -0700

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const frequencies = Array.from({ length: nums.length + 1 }, () => []);
+        const freqMap = {};
+
+        for (const num of nums) {
+            freqMap[num] = (freqMap[num] || 0) + 1;
+        }
+
+        for (const [num, freq] of Object.entries(freqMap)) {
+            frequencies[freq].push(parseInt(num, 10));
+        }
+        
+        const res = [];
+        for (let i = frequencies.length - 1; i >= 0; i -= 1) {
+            for (const num of frequencies[i]) {
+                res.push(num);
+                if (res.length === k) return res;
+            }
+        }
+    }
+}
